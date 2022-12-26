6c47d0262ed5673ec88b41da4e2b15097c401c37
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-26 15:08:43 +0900

    Middleware next 사용법

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ app.set('port', process.env.PORT || 3000);
 app.use((req, res, next) => {
     console.log('모든 요청에 실해하고 싶어요.');
     next();
-});
+}
+// ,(req, res, next) => {
+//     try {
+//         console.log('에러 코드');
+//         throw new Error('에러 코드');
+//     } catch(error) {
+//         next(error);
+//         // 에러 발생시 `next(error)` 를 사용한다.
+//         //  * argument 가 없는 경우 **다음 middleware**
+//         //  * argument 가 있는 경우 **에러 처리 middleware**
+//     }
+// }
+);
 
 app.get('/error', (req, res) => {
     throw new Error();
@@ -30,8 +42,24 @@ app.get('/category*', (req, res) => {
     res.send('hello * ');
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+    if (true) {
+        next('route'); // true: 아래의 메소드(미들웨어) 가 실행됨 - res.json
+    } else {
+        next(); // false: 아래의 핸들러 처리가 실행됨. - res.sendFile
+    }
+}, (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+    console.log('false');
+});
+
+app.get('/', (req, res) => {
+    console.log('true');
+    // res.sendFile(path.join(__dirname, 'index.html'));
+    res.json({ hello: 'zerocho' });
+    // res.render('index');
+
+    console.log('hello zerocho'); // res.json 은 리턴이 아니다. 이 콘솔도 실행됨.
 });
 
 app.get('/about', (req, res) => {
@@ -53,6 +81,17 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
     console.log(`3000 번 포트로 서버가 실행됩니다.`);
 });
+
+// ## CH 6-5
+// ### `next()` 를 호출해야 다음 코드로(넘어감)
+// * next 를 주석 처리하면 응답이 전송되지 않음.
+// * 다음 미들웨어(라우터 미들웨어)로 넘어가지 않기 때문
+// * next 에 인수를 값을 넣으면 `에러 핸들러` 로 넘어감('route' 인 경우 다음 라우터로)
+//
+// 요청 -> morgan -next()-> static -next()-> json, urlencoded -next()-> cookieParser -next()-> 라우터 -next()-> 에러 처리 미들웨어 -> 응답
+// * next() 다음 미들웨어로
+// * next('router') 다음 라우터로
+// * next(error) 에러 핸들러로
 
 // ## CH 6-3
 // ### app.use(...), app.get(...),
